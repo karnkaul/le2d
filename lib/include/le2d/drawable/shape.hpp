b5daedebd2a3211100a5a17d5c5fa4d006b75590
Karn Kaul <karnkaul@gmail.com>
2025-02-23 17:27:07 -0800

    Add `shape::Triangle`

--- lib/include/le2d/drawable/shape.hpp
+++ lib/include/le2d/drawable/shape.hpp
@@ -2,8 +2,12 @@
 #include <le2d/drawable/geometry.hpp>
 #include <le2d/shape/quad.hpp>
 #include <le2d/shape/super_ellipse.hpp>
+#include <le2d/shape/triangle.hpp>
 
 namespace le::drawable {
+using Triangle = Geometry<shape::Triangle>;
+using InstancedTriangle = InstancedGeometry<shape::Triangle>;
+
 using Quad = Geometry<shape::Quad>;
 using InstancedQuad = InstancedGeometry<shape::Quad>;
 
---
+++ lib/include/le2d/shape/triangle.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <kvf/color.hpp>
+#include <kvf/rect.hpp>
+#include <le2d/geometry.hpp>
+#include <array>
+
+namespace le::shape {
+class Triangle : public IGeometry {
+  public:
+	static constexpr std::size_t vertex_count_v{4};
+
+	[[nodiscard]] auto get_vertices() const -> std::span<Vertex const> final { return vertices; }
+	[[nodiscard]] auto get_indices() const -> std::span<std::uint32_t const> final { return {}; }
+	[[nodiscard]] auto get_topology() const -> vk::PrimitiveTopology final { return vk::PrimitiveTopology::eTriangleList; }
+
+	void create(float radius = 100.0f, kvf::Color color = kvf::white_v);
+
+	std::array<Vertex, vertex_count_v> vertices{};
+};
+} // namespace le::shape
