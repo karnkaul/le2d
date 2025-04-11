cffc86d54672f5e36288df9b6d1e552f59b51665
Karn Kaul <karnkaul@gmail.com>
2025-04-11 16:45:20 -0700

    kvf update, cleanup

--- lib/include/le2d/shape/circle.hpp
+++ lib/include/le2d/shape/circle.hpp
@@ -5,31 +5,21 @@
 #include <le2d/vertex_array.hpp>
 
 namespace le::shape {
-struct CircleParams {
-	float diameter{200.0f};
-	kvf::Color color{kvf::white_v};
-	std::int32_t resolution{128};
-};
-
 class Circle : public IGeometry {
   public:
-	using Params = CircleParams;
-
 	[[nodiscard]] auto get_vertices() const -> std::span<Vertex const> final { return m_verts.vertices; }
 	[[nodiscard]] auto get_indices() const -> std::span<std::uint32_t const> final { return m_verts.indices; }
 	[[nodiscard]] auto get_topology() const -> vk::PrimitiveTopology final { return vk::PrimitiveTopology::eTriangleFan; }
 
-	void create(Params const& params = {});
-	void create(float const diameter = 200.0f) { create(Params{.diameter = diameter}); }
+	void create(float diameter, kvf::Color color = kvf::white_v, std::int32_t resolution = 128);
 
-	[[nodiscard]] auto get_params() const -> Params const& { return m_params; }
-	[[nodiscard]] auto get_diameter() const -> float { return get_params().diameter; }
+	[[nodiscard]] auto get_diameter() const -> float { return m_diameter; }
 	[[nodiscard]] auto get_size() const -> glm::vec2 { return glm::vec2{get_diameter()}; }
 
 	[[nodiscard]] auto get_vertex_array() const -> VertexArray const& { return m_verts; }
 
   private:
 	VertexArray m_verts{};
-	Params m_params{};
+	float m_diameter{};
 };
 } // namespace le::shape
--- lib/src/shape/circle.cpp
+++ lib/src/shape/circle.cpp
@@ -3,19 +3,19 @@
 #include <cmath>
 
 namespace le::shape {
-void Circle::create(Params const& params) {
+void Circle::create(float const diameter, kvf::Color const color, std::int32_t const resolution) {
 	m_verts.clear();
-	m_params = params;
+	m_diameter = diameter;
 
-	auto const color = params.color.to_linear();
-	auto const step = 360.0f / float(params.resolution);
-	m_verts.vertices.push_back(Vertex{.color = color, .uv = glm::vec2{0.5f}});
-	auto const r = m_params.diameter * 0.5f;
+	auto const fcolor = color.to_linear();
+	auto const step = 360.0f / float(resolution);
+	m_verts.vertices.push_back(Vertex{.color = fcolor, .uv = glm::vec2{0.5f}});
+	auto const r = diameter * 0.5f;
 	for (auto angle = 0.0f; angle < 360.0f + step; angle += step) {
 		auto const theta = glm::radians(angle);
 		auto const cos = std::cos(theta);
 		auto const sin = std::sin(theta);
-		auto vertex = Vertex{.position = {r * cos, r * sin}, .color = color};
+		auto vertex = Vertex{.position = {r * cos, r * sin}, .color = fcolor};
 		vertex.uv = {cos + 0.5f, 0.5f - sin};
 		m_verts.vertices.push_back(vertex);
 	}
