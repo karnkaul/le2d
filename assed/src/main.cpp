586b0b9e9f1fd49b86778621fd3e0941f062aec5
Karn Kaul <karnkaul@gmail.com>
2026-06-26 07:54:59 -0700

    Cleanup

--- assed/src/main.cpp
+++ assed/src/main.cpp
@@ -19,10 +19,13 @@ auto main(int argc, char** argv) -> int {
 	try {
 		auto assets_dir = std::string{};
 		auto force_x11 = false;
-		auto spec = clap::spec::Parameters{.parameters = {
-											   clap::named_flag(force_x11, "x,force-x11", "Force X11 backend"),
-											   clap::named_option(assets_dir, "a,assets", "path to assets directory"),
-										   }};
+		auto spec = clap::spec::Parameters{
+			.parameters =
+				{
+					clap::named_flag(force_x11, "x,force-x11", "Force X11 backend"),
+					clap::named_option(assets_dir, "a,assets", "path to assets directory"),
+				},
+		};
 		auto parser = clap::Parser{std::move(spec)};
 		auto const parse_result = parser.parse_main(argc, argv);
 		if (parse_result.should_early_exit()) { return parse_result.return_code(); }
