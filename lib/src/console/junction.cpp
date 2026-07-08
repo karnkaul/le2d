d94a0091debeef1f838a7d3946e2f9c43bff7def
Karn Kaul <karnkaul@gmail.com>
2026-07-07 20:53:31 -0700

    `console::Junction`: optional `Terminal`

--- lib/src/console/junction.cpp
+++ lib/src/console/junction.cpp
@@ -1,10 +1,12 @@
 #include "le2d/console/junction.hpp"
-#include "kvf/is_positive.hpp"
 
 namespace le::console {
 void Junction::dispatch(std::span<Event const> events, glm::ivec2 const framebuffer_size) const {
-	auto const state_change = m_terminal->handle_events(framebuffer_size, events);
-	if (state_change == StateChange::Activated) { m_router->disengage(); }
-	if (!m_terminal->is_active()) { m_router->dispatch(events); }
+	if (m_terminal) {
+		auto const state_change = m_terminal->handle_events(framebuffer_size, events);
+		if (state_change == StateChange::Activated) { m_router->disengage(); }
+		if (m_terminal->is_active()) { return; }
+	}
+	m_router->dispatch(events);
 }
 } // namespace le::console
